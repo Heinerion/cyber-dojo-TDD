05387f8d247df23675c671d10484a02edd397b20
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-09 13:31:47 +0100

    add citation from readme to test case

--- src/test/java/de/hsp/tdd/_100_doors/HundredDoorsTest.java
+++ src/test/java/de/hsp/tdd/_100_doors/HundredDoorsTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class HundredDoorsTest {
+  /**
+   * 100 doors in a row are all initially closed.
+   */
   @Test
   void all_doors_are_initially_closed() {
     assertEquals(0, new HundredDoors().countOpenDoors());
