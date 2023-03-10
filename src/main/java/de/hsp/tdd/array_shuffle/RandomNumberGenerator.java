a1456030e01cfd94138f4450641833194dff3ccd
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-10 15:06:20 +0100

    swap arguments if mixed up

--- src/main/java/de/hsp/tdd/array_shuffle/RandomNumberGenerator.java
+++ src/main/java/de/hsp/tdd/array_shuffle/RandomNumberGenerator.java
@@ -2,6 +2,9 @@ package de.hsp.tdd.array_shuffle;
 
 public class RandomNumberGenerator {
   public int getRandom(int min, int maxExcl) {
+    if (maxExcl < min) {
+      return getRandom(maxExcl, min);
+    }
     return min;
   }
 }
--- src/test/java/de/hsp/tdd/array_shuffle/ArrayShuffleTest.java
+++ src/test/java/de/hsp/tdd/array_shuffle/ArrayShuffleTest.java
@@ -17,6 +17,11 @@ class ArrayShuffleTest {
   }
 
   @Test
+  void random_swaps_arguments_if_max_is_less_than_min() {
+    assertEquals(2, new RandomNumberGenerator().getRandom(3, 2));
+  }
+
+  @Test
   void random_returns_min_if_min_and_max_are_equal() {
     assertEquals(2, new RandomNumberGenerator().getRandom(2, 2));
   }
