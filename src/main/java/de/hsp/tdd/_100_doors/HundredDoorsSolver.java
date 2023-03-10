15ce2fdc1de10075de184cb1dbc93a5b385bc88a
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-10 12:43:54 +0100

    extract the run-method in its own class

---
+++ src/main/java/de/hsp/tdd/_100_doors/HundredDoorsSolver.java
@@ -0,0 +1,40 @@
+package de.hsp.tdd._100_doors;
+
+import java.util.*;
+
+public class HundredDoorsSolver {
+  /**
+   * <pre>
+   * 100 doors in a row are all initially closed. You make 100 passes by the doors. The first time through, you visit every door and toggle the door (if the door is closed, you open it; if it is open, you close it).
+   * The second time you only visit every 2nd door (door #2, #4, #6, ...).
+   * The third time, every 3rd door (door #3, #6, #9, ...), etc, until you only visit the 100th door.
+   *
+   * Question: What state are the doors in after the last pass? Which are open, which are closed?
+   *
+   * [Source http://rosettacode.org]
+   * </pre>
+   */
+  public static void main(String... args) {
+    HundredDoors run = new HundredDoors().run(100);
+    long openDoorCount = run.countOpenDoors();
+
+    List<String> openDoors = new ArrayList<>();
+    List<String> closedDoors = new ArrayList<>();
+    for (Door door : run.getDoors()) {
+      if (door.isOpen()) {
+        openDoors.add(door.toString());
+      } else {
+        closedDoors.add(door.toString());
+      }
+    }
+
+    // Question: What state are the doors in after the last pass? Which are open, which are closed?
+    System.out.printf(
+        "After one hundred passes, %d doors are open.%n"
+            + "Open doors:    %s%n"
+            + "Closed doors:  %s%n",
+        openDoorCount,
+        String.join(", ", openDoors),
+        String.join(", ", closedDoors));
+  }
+}
