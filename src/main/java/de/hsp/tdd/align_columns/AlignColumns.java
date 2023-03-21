20b500e2c118603915618195807d8162d91916af
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-21 13:53:40 +0100

    extract some constants

--- src/main/java/de/hsp/tdd/align_columns/AlignColumns.java
+++ src/main/java/de/hsp/tdd/align_columns/AlignColumns.java
@@ -3,6 +3,24 @@ package de.hsp.tdd.align_columns;
 import java.util.*;
 
 public class AlignColumns {
+  /**
+   * REGEX describing how lines in incoming text are separated
+   */
+  public static final String LINE_SEPARATOR_REGEX = "\\n";
+  /**
+   * Delimiter to separate lines in output
+   */
+  public static final String LINE_DELIMITER = "\n";
+
+  /**
+   * REGEX describing how word in a single line are separated in incoming text
+   */
+  public static final String WORD_SEPARATOR_REGEX = "\\$";
+  /**
+   * Delimiter to separate words in output
+   */
+  public static final String WORD_DELIMITER = " ";
+
   private final String[] lines;
   private final String[][] wordTable;
   private final int[] column2width;
@@ -10,7 +28,7 @@ public class AlignColumns {
   private int maxCol;
 
   public AlignColumns(String source) {
-    lines = source.split("\\n");
+    lines = source.split(LINE_SEPARATOR_REGEX);
     wordTable = new String[lines.length][255];
     column2width = new int[255];
 
@@ -24,7 +42,7 @@ public class AlignColumns {
 
   private void analyzeLines() {
     for (int row = 0; row < lines.length; row++) {
-      String[] words = lines[row].split("\\$");
+      String[] words = lines[row].split(WORD_SEPARATOR_REGEX);
       for (int column = 0; column < words.length; column++) {
         analyzeColumn(words[column], row, column);
       }
@@ -45,7 +63,7 @@ public class AlignColumns {
       formattedLines.add(formatLine(row, column2width, maxCol));
     }
 
-    return String.join("\n", formattedLines);
+    return String.join(LINE_DELIMITER, formattedLines);
   }
 
   private String formatLine(String[] row, int[] column2width, int maxCol) {
@@ -55,7 +73,7 @@ public class AlignColumns {
       line.add(formatWord(row[column], column2width[column]));
     }
 
-    return String.join(" ", line).trim();
+    return String.join(WORD_DELIMITER, line).trim();
   }
 
   private String formatWord(String word, int width) {
