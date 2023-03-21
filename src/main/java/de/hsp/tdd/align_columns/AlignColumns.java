b6979bbdd5bd351641a7605e732fe921f7a6a16a
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-21 13:26:37 +0100

    refactor formatting of output

--- src/main/java/de/hsp/tdd/align_columns/AlignColumns.java
+++ src/main/java/de/hsp/tdd/align_columns/AlignColumns.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class AlignColumns {
   public String align(String source) {
     String[] lines = source.split("\\n");
-    String[][] wordMatrix = new String[lines.length][255];
+    String[][] wordTable = new String[lines.length][255];
     int[] column2width = new int[255];
 
     int maxCol = 0;
@@ -15,24 +15,37 @@ public class AlignColumns {
       for (int column = 0; column < words.length; column++) {
         String word = words[column];
         column2width[column] = Math.max(word.length(), column2width[column]);
-        wordMatrix[lineNumber][column] = word;
+        wordTable[lineNumber][column] = word;
 
         maxCol = Math.max(column, maxCol);
       }
     }
 
+    return formatLines(wordTable, column2width, maxCol);
+  }
+
+  private String formatLines(String[][] wordTable, int[] column2width, int maxCol) {
     List<String> formattedLines = new ArrayList<>();
-    for (String[] matrix : wordMatrix) {
-      StringBuilder lineBuilder = new StringBuilder();
-      for (int column = 0; column <= maxCol; column++) {
-        String word = matrix[column];
-        String format = "%-" + column2width[column] + "s";
-        lineBuilder.append(String.format(format, word == null ? "" : word));
-        lineBuilder.append(" ");
-      }
-      formattedLines.add(lineBuilder.toString().trim());
+
+    for (String[] row : wordTable) {
+      formattedLines.add(formatLine(row, column2width, maxCol));
     }
 
     return String.join("\n", formattedLines);
+  }
+
+  private String formatLine(String[] row, int[] column2width, int maxCol) {
+    List<String> line = new ArrayList<>();
+
+    for (int column = 0; column <= maxCol; column++) {
+      line.add(formatWord(row[column], column2width[column]));
+    }
+
+    return String.join(" ", line).trim();
+  }
+
+  private String formatWord(String word, int width) {
+    String format = "%-" + width + "s";
+    return String.format(format, Optional.ofNullable(word).orElse(""));
   }
 }
