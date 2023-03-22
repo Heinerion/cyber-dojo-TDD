4c2d25c63563c187ca2a0cfa850c3c11fdf66a49
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-22 13:17:49 +0100

    choose positive number, if a tie occurs

--- src/main/java/de/hsp/tdd/closest_to_zero/CloseToZeroDetector.java
+++ src/main/java/de/hsp/tdd/closest_to_zero/CloseToZeroDetector.java
@@ -4,7 +4,13 @@ public class CloseToZeroDetector {
   public int detectClosestNumber(int... numbers) {
     int smallest = Integer.MAX_VALUE;
     for (int number : numbers) {
-      if (Math.abs(number) < Math.abs(smallest)) {
+      int distance = Math.abs(number);
+      int smallestDistance = Math.abs(smallest);
+      if (distance < smallestDistance) {
+        smallest = number;
+      } else if (distance == smallestDistance
+          // If the distance to zero is equal, choose the positive value
+          && number > smallest) {
         smallest = number;
       }
     }
--- src/test/java/de/hsp/tdd/closest_to_zero/CloseToZeroDetectorTest.java
+++ src/test/java/de/hsp/tdd/closest_to_zero/CloseToZeroDetectorTest.java
@@ -35,4 +35,14 @@ class CloseToZeroDetectorTest {
     int closest = new CloseToZeroDetector().detectClosestNumber(7, -2, 13, -1);
     assertEquals(-1, closest);
   }
+
+  @Test
+  void treat_positive_numbers_as_closer_to_zero() {
+    int closest = new CloseToZeroDetector().detectClosestNumber(-1, 1, -1);
+    assertEquals(1, closest);
+
+    // second check: make sure, that the order of the arguments has no effect
+    closest = new CloseToZeroDetector().detectClosestNumber(1, -1, 1);
+    assertEquals(1, closest);
+  }
 }
