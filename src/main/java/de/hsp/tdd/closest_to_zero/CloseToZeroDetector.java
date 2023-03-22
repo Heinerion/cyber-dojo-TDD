e10a21b3f4779e12bd9c2526765179c2e1fdbb95
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-22 13:04:55 +0100

    delete unnecessary if

--- src/main/java/de/hsp/tdd/closest_to_zero/CloseToZeroDetector.java
+++ src/main/java/de/hsp/tdd/closest_to_zero/CloseToZeroDetector.java
@@ -4,9 +4,6 @@ import java.util.stream.IntStream;
 
 public class CloseToZeroDetector {
   public int detectClosestNumber(int... numbers) {
-    if (numbers.length == 0) {
-      return Integer.MAX_VALUE;
-    }
     return IntStream
         .of(numbers)
         .min()
