f5900bdcd6d57f50d5ed03a2529f045b997a1d7e
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-22 11:44:14 +0100

    refactor Statistics

--- src/main/java/de/hsp/tdd/calc_stats/Statistics.java
+++ src/main/java/de/hsp/tdd/calc_stats/Statistics.java
@@ -1,6 +1,11 @@
 package de.hsp.tdd.calc_stats;
 
-public class Statistics {
+public final class Statistics {
+
+  private Statistics() {
+    // hide default constructor
+  }
+
   public static Statistics of(int i) {
     return new Statistics();
   }
