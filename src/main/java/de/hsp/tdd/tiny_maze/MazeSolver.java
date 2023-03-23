5e00f21d14033d050d3db6f0f4f22e0c4ea3af87
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-23 09:28:58 +0100

    extract constants

--- src/main/java/de/hsp/tdd/tiny_maze/MazeSolver.java
+++ src/main/java/de/hsp/tdd/tiny_maze/MazeSolver.java
@@ -1,15 +1,19 @@
 package de.hsp.tdd.tiny_maze;
 
 public class MazeSolver {
+
+  public static final String WALL = "1";
+  public static final String VISITED = "x";
+
   public String[][] solve(String[][] maze) {
     String[][] result = new String[maze.length][maze[0].length];
     for (int row = 0, resultLength = result.length; row < resultLength; row++) {
       for (int column = 0, rowLength = result[row].length; column < rowLength; column++) {
         String field = maze[row][column];
-        if (field.equalsIgnoreCase("1")) {
+        if (field.equalsIgnoreCase(WALL)) {
           result[row][column] = field;
         } else {
-          result[row][column] = "x";
+          result[row][column] = VISITED;
         }
       }
     }
