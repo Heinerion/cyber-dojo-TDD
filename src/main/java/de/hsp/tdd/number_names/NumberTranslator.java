816a5c82e54585eb0a805878db76d60871d6556b
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-26 16:05:39 +0200

    translate up to nineteen

--- src/main/java/de/hsp/tdd/number_names/NumberTranslator.java
+++ src/main/java/de/hsp/tdd/number_names/NumberTranslator.java
@@ -3,7 +3,42 @@ package de.hsp.tdd.number_names;
 public class NumberTranslator {
 
   public String translate(int i) {
-    String text = "";
+    return i < 10
+        ? translateDigit(i)
+        : translateTens(i);
+  }
+
+  private String translateTens(int i) {
+    String text;
+
+    switch (i) {
+      case 10:
+        text = "ten";
+        break;
+      case 11:
+        text = "eleven";
+        break;
+      case 12:
+        text = "twelve";
+        break;
+      case 13:
+        text = "thirteen";
+        break;
+      case 15:
+        text = "fifteen";
+        break;
+      case 18:
+        text = "eighteen";
+        break;
+      default:
+        text = translateDigit(i % 10) + "teen";
+    }
+
+    return text;
+  }
+
+  private String translateDigit(int i) {
+    String text;
 
     switch (i) {
       case 1:
@@ -33,6 +68,8 @@ public class NumberTranslator {
       case 9:
         text = "nine";
         break;
+      default:
+        text = "-";
     }
 
     return text;
--- src/test/java/de/hsp/tdd/number_names/NumberTranslatorTest.java
+++ src/test/java/de/hsp/tdd/number_names/NumberTranslatorTest.java
@@ -7,48 +7,97 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class NumberTranslatorTest {
   @Test
-  void translates_1_to_one() {
+  void translates_1() {
     assertEquals("one", new NumberTranslator().translate(1));
   }
 
   @Test
-  void translates_2_to_two() {
+  void translates_2() {
     assertEquals("two", new NumberTranslator().translate(2));
   }
 
   @Test
-  void translates_3_to_three() {
+  void translates_3() {
     assertEquals("three", new NumberTranslator().translate(3));
   }
 
   @Test
-  void translates_4_to_four() {
+  void translates_4() {
     assertEquals("four", new NumberTranslator().translate(4));
   }
 
   @Test
-  void translates_5_to_five() {
+  void translates_5() {
     assertEquals("five", new NumberTranslator().translate(5));
   }
 
   @Test
-  void translates_6_to_six() {
+  void translates_6() {
     assertEquals("six", new NumberTranslator().translate(6));
   }
 
   @Test
-  void translates_7_to_seven() {
+  void translates_7() {
     assertEquals("seven", new NumberTranslator().translate(7));
   }
 
   @Test
-  void translates_8_to_eight() {
+  void translates_8() {
     assertEquals("eight", new NumberTranslator().translate(8));
   }
 
   @Test
-  void translates_9_to_nine() {
+  void translates_9() {
     assertEquals("nine", new NumberTranslator().translate(9));
   }
 
+  @Test
+  void translates_10() {
+    assertEquals("ten", new NumberTranslator().translate(10));
+  }
+
+  @Test
+  void translates_11() {
+    assertEquals("eleven", new NumberTranslator().translate(11));
+  }
+
+  @Test
+  void translates_12() {
+    assertEquals("twelve", new NumberTranslator().translate(12));
+  }
+
+  @Test
+  void translates_13() {
+    assertEquals("thirteen", new NumberTranslator().translate(13));
+  }
+
+  @Test
+  void translates_14() {
+    assertEquals("fourteen", new NumberTranslator().translate(14));
+  }
+
+  @Test
+  void translates_15() {
+    assertEquals("fifteen", new NumberTranslator().translate(15));
+  }
+
+  @Test
+  void translates_16() {
+    assertEquals("sixteen", new NumberTranslator().translate(16));
+  }
+
+  @Test
+  void translates_17() {
+    assertEquals("seventeen", new NumberTranslator().translate(17));
+  }
+
+  @Test
+  void translates_18() {
+    assertEquals("eighteen", new NumberTranslator().translate(18));
+  }
+
+  @Test
+  void translates_19() {
+    assertEquals("nineteen", new NumberTranslator().translate(19));
+  }
 }
