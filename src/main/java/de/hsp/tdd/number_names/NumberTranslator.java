8d3e051bd125c93d43325f8ce73200fbf49a5481
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-03-26 15:53:26 +0200

    translate numbers 2 thru 9

--- src/main/java/de/hsp/tdd/number_names/NumberTranslator.java
+++ src/main/java/de/hsp/tdd/number_names/NumberTranslator.java
@@ -1,7 +1,40 @@
 package de.hsp.tdd.number_names;
 
 public class NumberTranslator {
+
   public String translate(int i) {
-    return "one";
+    String text = "";
+
+    switch (i) {
+      case 1:
+        text = "one";
+        break;
+      case 2:
+        text = "two";
+        break;
+      case 3:
+        text = "three";
+        break;
+      case 4:
+        text = "four";
+        break;
+      case 5:
+        text = "five";
+        break;
+      case 6:
+        text = "six";
+        break;
+      case 7:
+        text = "seven";
+        break;
+      case 8:
+        text = "eight";
+        break;
+      case 9:
+        text = "nine";
+        break;
+    }
+
+    return text;
   }
 }
--- src/test/java/de/hsp/tdd/number_names/NumberTranslatorTest.java
+++ src/test/java/de/hsp/tdd/number_names/NumberTranslatorTest.java
@@ -10,4 +10,45 @@ class NumberTranslatorTest {
   void translates_1_to_one() {
     assertEquals("one", new NumberTranslator().translate(1));
   }
+
+  @Test
+  void translates_2_to_two() {
+    assertEquals("two", new NumberTranslator().translate(2));
+  }
+
+  @Test
+  void translates_3_to_three() {
+    assertEquals("three", new NumberTranslator().translate(3));
+  }
+
+  @Test
+  void translates_4_to_four() {
+    assertEquals("four", new NumberTranslator().translate(4));
+  }
+
+  @Test
+  void translates_5_to_five() {
+    assertEquals("five", new NumberTranslator().translate(5));
+  }
+
+  @Test
+  void translates_6_to_six() {
+    assertEquals("six", new NumberTranslator().translate(6));
+  }
+
+  @Test
+  void translates_7_to_seven() {
+    assertEquals("seven", new NumberTranslator().translate(7));
+  }
+
+  @Test
+  void translates_8_to_eight() {
+    assertEquals("eight", new NumberTranslator().translate(8));
+  }
+
+  @Test
+  void translates_9_to_nine() {
+    assertEquals("nine", new NumberTranslator().translate(9));
+  }
+
 }
